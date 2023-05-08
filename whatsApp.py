dc783ceffc2e601c7c97b36b075ea0fad73bd7e5
Praveenmittakadapala8794 <mittakadapala.praveen@gmail.com>
2023-05-08 12:00:01 +0530

    This is My WhatsApp messages sending Project

---
+++ whatsApp.py
@@ -0,0 +1,12 @@
+import pywhatkit as msg
+msg.sendwhatmsg("+91 9121066797","oothagane send chesna,ignore this message",13,20)
+
+#one more type also available
+
+# import pywhatkit as msg
+# import getpass as gp
+# print("Let's Automate whatsapp..")
+
+# p_num=gp.getpass(prompt="phoneNumber:",stream=None)
+# message = "same atlane send chesa,dont react this"
+# msg.sendwhatmsg(p_num,message,13,38)
